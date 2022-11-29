84471f9d263210bb314bd1582554044f0ff1e2d0
PtFux <91057700+PtFux@users.noreply.github.com>
2022-11-29 22:04:44 +0300

    Add files

---
+++ C_test.py
@@ -0,0 +1,12 @@
+from solution import PearBasket
+
+pb = PearBasket(17)
+array = pb // 4
+print(array)
+
+pb_2 = PearBasket(13)
+pb_3 = pb + pb_2
+print(pb_3)
+print(pb_3 % 7)
+
+print(PearBasket(15) // 1)
---
+++ solution.py
@@ -0,0 +1,31 @@
+class PearBasket:
+    def __init__(self, amount: int):
+        if amount > 0:
+            self.amount = amount
+        else:
+            self.amount = 0
+
+    def __add__(self, other):
+        if type(other) is PearBasket:
+            return PearBasket(self.amount + other.amount)
+
+    def __sub__(self, other):
+        if type(other) is int:
+            self.amount = max(0, self.amount - other)
+            return self
+
+    def __floordiv__(self, other):
+        if type(other) is int:
+            baskets = [PearBasket(self.amount//other) for i in range(other)]
+            if self.amount % other:
+                baskets.append(PearBasket(self.amount % other))
+            return baskets
+
+    def __mod__(self, other):
+        return self.amount % other
+
+    def __repr__(self):
+        return f"PearsBasket({self.amount})"
+
+    def __str__(self):
+        return str(self.amount)
